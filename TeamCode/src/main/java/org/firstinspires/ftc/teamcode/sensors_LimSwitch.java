2cc4c1f01e9e083f99f938c258d793c1deb3e6e7
calstar9000 <calstar9000@gmail.com>
2024-09-30 17:12:20 -0600

    Lim Switch first attempt

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensors_LimSwitch.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "sensor_LimSwitch", group = "Sensor")
+public class sensors_LimSwitch extends OpMode {
+
+    // Create LED object
+    RevBlinkinLedDriver blinkinLedDriver;
+
+    // Create pattern for LED
+    RevBlinkinLedDriver.BlinkinPattern pattern;
+
+    // Declare the DigitalChannel object for the limit switch
+    private DigitalChannel limitSwitch;
+
+    @Override
+    public void init() {
+        // Initialize the limit switch from the hardware map
+        limitSwitch = hardwareMap.get(DigitalChannel.class, "limitSwitch");
+
+        // Set the limit switch as an input device
+        limitSwitch.setMode(DigitalChannel.Mode.INPUT);
+
+        // Assign the LED to the object
+        blinkinLedDriver = hardwareMap.get(RevBlinkinLedDriver.class, "blinkin");
+    }
+
+    @Override
+    public void loop() {
+        // Read the state of the limit switch
+        boolean isPressed = limitSwitch.getState(); /* (True when pressed, false when not) */
+        //Error check for the LED
+        if (blinkinLedDriver != null) {
+            //Error check for the switch
+            if (limitSwitch != null){
+                if (isPressed) {
+                    // If activated, turn green
+                    pattern = RevBlinkinLedDriver.BlinkinPattern.GREEN;
+                    blinkinLedDriver.setPattern(pattern);
+                } else {
+                    // If not activated, turn red
+                    pattern = RevBlinkinLedDriver.BlinkinPattern.RED;
+                    blinkinLedDriver.setPattern(pattern);
+                }
+            }
+
+            else {
+                telemetry.addData("Limit Switch Broken, ", limitSwitch);
+                telemetry.update();
+            }
+        } else {
+            telemetry.addData("LED Broken, ", blinkinLedDriver);
+            telemetry.update();
+        }
+    }
+}
